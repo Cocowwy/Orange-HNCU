bcc9799694f82ea7226dd60e91f98d5f56a481d8
Cocowwy <514658459@qq.com>
2020-12-25 14:42:12 +0800

    初始化项目

---
+++ orange-order/src/main/java/cn/cocowwy/orange/orangeorder/OrangeOrderApplication.java
@@ -0,0 +1,13 @@
+package cn.cocowwy.orange.orangeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeOrderApplication.class, args);
+    }
+
+}
---
+++ orange-wall/src/main/java/cn/cocowwy/orange/orangewall/OrangeWallApplication.java
@@ -0,0 +1,13 @@
+package cn.cocowwy.orange.orangewall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeWallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeWallApplication.class, args);
+    }
+
+}
